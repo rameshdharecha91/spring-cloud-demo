c51936460168564f8f0a52d468bcdb7c88daf2c7
Dharecha Rameshkumar Devshibhai <rameshdharecha91@gmail.com>
2024-01-24 23:15:27 +0530

    API gateway

---
+++ api-gatewaye/src/main/java/com/apigateway/ApiGatewayeApplication.java
@@ -0,0 +1,13 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayeApplication.class, args);
+	}
+
+}
